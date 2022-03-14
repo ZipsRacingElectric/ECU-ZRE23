bb57c0a92f5d0259ba25e8bb5c9a120ae25b79c0
Derek Dunn <derek.dunn001@gmail.com>
2022-03-14 12:44:24 -0400

    Add pump and hv control relay logic

--- ECU22.X/state_manager.c
+++ ECU22.X/state_manager.c
@@ -56,11 +56,13 @@ void update_vehicle_mode(VEHICLE_MODES dash_mode)
 
 void set_ready_to_drive()
 {
+    // enter ready to drive if the below conditions are met
     if (HV_ON_GetValue() && car_data.is_braking && !car_data.ready_to_drive 
             && !car_data.accelerator_is_pressed && car_data.mode != NAW)
     {
         start_RTD_buzzer();
         car_data.ready_to_drive = true;
+        PUMP_CTRL_SetHigh();
         LED4_SetHigh();
     }
 }
@@ -68,5 +70,6 @@ void set_ready_to_drive()
 void exit_ready_to_drive()
 {
     car_data.ready_to_drive = false;
+    PUMP_CTRL_SetLow();
     LED4_SetLow();
 }
